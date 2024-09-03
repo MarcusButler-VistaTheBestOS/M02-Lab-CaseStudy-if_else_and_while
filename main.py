4d0bd793f80e6bfeb0c12be03725f0c3cd3d34e4
unknown <mbutler147@ivytech.edu>
2024-09-03 10:29:16 -0500

    Corrected some mistakes like not telling the user how to exit the program and only to print out if they made the Dean's list and Honor Roll and not print that they made both.

--- main.py
+++ main.py
@@ -11,28 +11,28 @@
 
 
 # GPA needed to be on Honor Roll or Dean's List.
-HONOR_ROLL = 3.25
 DEANS_LIST = 3.5
+HONOR_ROLL = 3.25
 
 
 """== MAIN LOOP =="""
 while True:
     # Ask for a student's last name. If last name == 'ZZZ' then terminate program.
-    last_name = input("\nWhat is the student's last name?\n\t>")
+    last_name = input("\nWhat is the student's last name? (Type 'ZZZ' to exit.)\n\t> ")
     if last_name == "ZZZ":
         break
 
     # Ask for a student's first name.
-    first_name = input("What is the student's first name?\n\t>")
+    first_name = input("What is the student's first name?\n\t> ")
     
     # Ask for a student's GPA as a float.
-    gpa = float(input("What is the students GPA?\n\t>"))
+    gpa = float(input("What is the students GPA?\n\t> "))
     
     # GPA >= 3.5 tell the user the student made the Dean's List.
     if gpa >= DEANS_LIST:
         print(f"{first_name} {last_name} made the Dean's List!")
     # GPA >= 3.25 tell the user the student made the Honor Roll.
-    if gpa >= HONOR_ROLL:
+    elif gpa >= HONOR_ROLL:
         print(f"{first_name} {last_name} made the Honor Roll!")
 
 # Runs on exit.
